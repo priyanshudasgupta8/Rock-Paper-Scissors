ca8ebb8038487f6e8ff9ee4c015831856e9c85f0
Priyanshu Dasgupta <62264051+priyanshudasgupta8@users.noreply.github.com>
2020-06-21 12:27:55 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+const selectionButtons = document.querySelectorAll('[data-selection]');
+const finalColumn = document.querySelector('[data-final-column]');
+const computerScoreSpan = document.querySelector('[data-computer-score]');
+const yourScoreSpan = document.querySelector('[data-your-score]');
+const SELECTIONS = [
+    {
+        name: 'rock',
+        emoji: '👊🏻',
+        beats: 'scissors'
+    },
+    {
+        name: 'paper',
+        emoji: '✋🏻',
+        beats: 'rock'
+    },
+    {
+        name: 'scissors',
+        emoji: '✌🏻',
+        beats: 'paper'
+    }
+];
+
+alert("Press CTRL + MOUSE WHEEL SCROLL UP until the page is at a zoom of 400%");
+
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection;
+        const selection = SELECTIONS.find(selection => selection.name === selectionName);
+        makeSelection(selection);
+    });
+});
+
+function makeSelection(selection) {
+    const computerSelection = randomSelection();
+    const yourWinner = isWinner(selection, computerSelection);
+    const computerWinner = isWinner(computerSelection, selection);
+
+    addSelectionResult(computerSelection, computerWinner);
+    addSelectionResult(selection, yourWinner);
+
+    if (yourWinner == true) {
+        incrementScore(yourScoreSpan);
+    }
+    if (computerWinner == true) {
+        incrementScore(computerScoreSpan);
+    }
+}
+
+function incrementScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1;
+}
+
+function addSelectionResult(selection, winner) {
+    const div = document.createElement('div');
+    div.innerText = selection.emoji;
+    div.classList.add('result-selection');
+    if (winner == true) {
+        div.classList.add('winner');
+    }
+    finalColumn.after(div);
+}
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name;
+}
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length);
+    return SELECTIONS[randomIndex];
+}
